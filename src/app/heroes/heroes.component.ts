6b8c92d8419c7277d8de9fc2f81e23206bdb2404
Andrew Ushakov <redstoniska.minecraft@gmail.com>
2021-04-07 23:07:04 +0300

    refactor: remove unnecessary imports

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
-import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
