26e4937d1f038eb28163bc4fc1c9ae46014c0696
Andrew Ushakov <redstoniska.minecraft@gmail.com>
2021-04-07 23:34:51 +0300

    feat: add button to go back

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,33 @@
+import { Location } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    if (!this.hero) this.getHero();
   }
 
+  getHero() {
+    const id = +(this.route.snapshot.paramMap.get('id') || 0);
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
